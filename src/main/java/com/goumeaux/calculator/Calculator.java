9b91b67b7be251698a5fa2f101eaeca342e920d1
Gauthier Goumeaux <ggoumeaux001@iutbayonne.univ-pau.fr>
2024-09-24 17:27:03 +0200

    Créations des méthodes + test

--- src/main/java/com/goumeaux/calculator/Calculator.java
+++ src/main/java/com/goumeaux/calculator/Calculator.java
@@ -1,5 +1,16 @@
 package com.goumeaux.calculator;
 
 public class Calculator {
+    public int add(int opG, int opD) {
+        return opG + opD;
+    };
+    public int divide(int opG, int opD) {
+        if (opD != 0) {
+            return opG / opD;
+        }
+        else {
+            throw new RuntimeException("Division par 0 impossible");
+        }
+    };
 }
 
--- src/test/java/com/goumeaux/calculator/CalculatorTest.java
+++ src/test/java/com/goumeaux/calculator/CalculatorTest.java
@@ -1,7 +1,46 @@
 package com.goumeaux.calculator;
 
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
+import org.assertj.core.api.Assertions;
 
 class CalculatorTest {
+    @Test
+    void add_devrait_calculer_la_somme_des_deux_operateurs() {
+        // GIVEN
+        int opG = 1;
+        int opD = 1;
+        Calculator calculator = new Calculator();
 
+        // WHEN
+        int resultat = calculator.add(opG, opD);
+        // THEN
+        assertThat(resultat).isEqualTo(opG + opD);
+    }
+
+    @Test
+    void divide_devrait_calculer_la_division_des_deux_operateurs() {
+         // GIVEN
+         int opG = 1;
+         int opD = 1;
+         Calculator calculator = new Calculator();
+         // WHEN
+         int resultat = calculator.divide(opG,opD);
+         // THEN
+         assertThat(resultat).isEqualTo(opG / opD);
+    }
+
+    @Test
+    void divide_devrait_lever_une_erreur_en_cas_de_division_par_zero() {
+        // GIVEN
+        int opG = 1;
+        int opD = 0;
+        Calculator calculator = new Calculator();
+        // WHEN
+        Throwable uneErreur = Assertions.catchThrowable(()->calculator.divide(opG,opD));
+        // THEN
+        assertThat(uneErreur).hasMessage("Division par 0 impossible");
+    }
 }
